c26cf3c9ed178283c33d9376de731a2b7af96f22
Luis Bazán <luis@bazan.pe>
2024-09-19 22:50:24 -0500

    Fix: Jorgitoocode bug

--- src/pages/Config.tsx
+++ src/pages/Config.tsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear } from '@fortawesome/free-solid-svg-icons';
-import Logo from '../components/atoms/logo';
-import Button from '../components/atoms/button';
-import { ThemeContext } from '../context/ThemeContext';
+import React, { useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../components/atoms/logo";
+import Button from "../components/atoms/button";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Config = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -11,17 +11,23 @@ const Config = () => {
   return (
     <div className="min-h-screen h-screen overflow-hidden flex flex-col items-center">
       <header className="w-full flex justify-between items-center p-4 bg-bg-color text-text-color">
-        <a href="/"><Logo logoType='secondary' /></a>
-        <a><FontAwesomeIcon icon={faGear} className="h-4 w-4 text-icon-color" /></a>
+        <a href="/">
+          <Logo logoType="secondary" />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faGear} className="h-4 w-4 text-icon-color" />
+        </a>
       </header>
       <main className="flex flex-col items-center w-full flex-1 p-4 bg-bg-color text-text-color">
         <div className="w-full flex flex-col items-center mb-4">
           <div className="border-t border-gray-300 w-full opacity-50 mb-4"></div>
           <div className="flex justify-between w-full">
             <span className="mr-2">Change theme</span>
-            <Button buttonType="secondary" onClick={toggleTheme}>
-              Toggle Theme
-            </Button>
+            <div className="w-1/3">
+              <Button buttonType="secondary" onClick={toggleTheme}>
+                Toggle Theme
+              </Button>
+            </div>
           </div>
         </div>
       </main>
